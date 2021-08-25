54e8c7174737b6e071ac87b29833502b2d121082
janeminty <44300123+janeminty@users.noreply.github.com>
2021-08-25 12:51:52 -0400

    Week 5 Lighting
    Week 5 lighting commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# unity-test
-another description
--- Week2-GitHub/Assets/Plugins/signin.bat
+++
@@ -1,3 +0,0 @@
-echo off
-title Sign In to AltspaceVR
-curl -v -d "user[email]=&user[password]=" https://account.altvr.com/users/sign_in.json -c cookie
